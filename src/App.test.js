35b97719447c14c6c843bf40bb85eed59409d1fa
madisondegrezia <madisondegrezia26@gmail.com>
2026-01-16 11:42:22 -0500

    Wrote unit tests using jest

--- src/App.test.js
+++ src/App.test.js
@@ -15,7 +15,13 @@ test('App should render', () => {
 
 test('Button should render', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+
+  const button = screen.getByRole('button', {
+    name: /current theme/i,
+  });
+
+  expect(button).toBeInTheDocument();
 });
 
 /**
@@ -24,7 +30,20 @@ test('Button should render', () => {
  */
 test('theme button should update button text', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+
+  const button = screen.getByRole('button', {
+    name: /current theme/i,
+  });
+
+  // initial theme
+  expect(button).toHaveTextContent(/light/i);
+
+  // fireEvent in its own test
+  fireEvent.click(button);
+
+  // theme after click
+  expect(button).toHaveTextContent(/dark/i);
 });
 
 // BONUS
@@ -32,7 +51,26 @@ test('theme button should update button text', () => {
 // e.g.: expect(element).toHaveStyle('color: #FFF');
 test('theme button should toggle styles', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+
+  const button = screen.getByRole('button', {
+    name: /current theme/i,
+  });
+
+  // initial styles (light theme)
+  expect(document.body).toHaveStyle({
+    backgroundColor: '#FFF',
+    color: '#333',
+  });
+
+  // click to toggle theme
+  fireEvent.click(button);
+
+  // updated styles (dark theme)
+  expect(document.body).toHaveStyle({
+    backgroundColor: '#333',
+    color: '#FFF',
+  });
 });
 
 /**
@@ -46,7 +84,32 @@ test('theme button should toggle styles', () => {
  */
 test('hidden button should toggle hidden content', () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+
+  // content should NOT be in the document initially
+  expect(
+    screen.queryByText(/this content is hidden by default/i)
+  ).not.toBeInTheDocument();
+
+  // click toggle button to show content
+  const toggleButton = screen.getByRole('button', {
+    name: /show hidden content/i,
+  });
+
+  fireEvent.click(toggleButton);
+
+  // content should now be visible
+  expect(
+    screen.getByText(/this content is hidden by default/i)
+  ).toBeInTheDocument();
+
+  // click toggle button to hide content
+  fireEvent.click(toggleButton);
+
+  // content should be hidden again
+  expect(
+    screen.queryByText(/this content is hidden by default/i)
+  ).not.toBeInTheDocument();
 });
 
 
